20b99b7c30bd561f4ed98a2048c3bc6920b1b455
Leon Johannessen <leon@plysjbyen.net>
2016-10-15 18:09:18 +0200

    tweaks to the sticky header javascript

--- Template/js/sticky-header.js
+++ Template/js/sticky-header.js
@@ -1,69 +1,91 @@
+/*jshint esversion: 6 */
+
 // Get the main-nav element
 const navbar = document.getElementById('main-nav');
 
-// Get the initial navbar position
-const stickyDistance = getDistance();
+// Method that initialize the sticky header
+function initializeStickyHeader(){
+  // Get the initial navbar position
+  const stickyDistance = navbar.offsetTop;
 
-// Set state flags
-var sticky = false;
-var scrolling = false;
+  //
+  console.assert(stickyDistance > 0, 'stickyDistance is not positive!');
 
-// Helper method to get distance from top to navbar
-function getDistance() {
-  return navbar.offsetTop;
-}
+  // Set state flags
+  var isScrolling = false;
 
-// Method that checks the current scroll position, and updates the navbar
-// state based on the scroll positon
-function updateNavbarState() {
-  // switch the "scrolling"-flag
-  scrolling = !scrolling;
+  // Method that checks the current scroll position, and updates the navbar
+  // state based on the scroll positon
+  function updateNavbarState() {
+    // get the current scroll position
+    const currentScrollPos = window.pageYOffset;
 
-  // get the current scroll position
-  var currentScrollPos = window.pageYOffset;
+    // get current distance from top of screen to navbar position
+    const currentDistanceToNavbar = stickyDistance - currentScrollPos;
 
-  // get current distance from top of screen to navbar position
-  var currentDistance = getDistance() - currentScrollPos;
+    // Boolean that evaluates to true if the user has scrolled past
+    // the initial navbar position
+    const hasScrolledPastNavBar = currentDistanceToNavbar <= 0;
 
-  if ((currentDistance <= 0) && !sticky) {
-    // if the user has scroleld past the navbar, and the navbar is not sticky,
-    // add the sticky class to it and flip the sticky flag
-    navbar.classList.add('sticky');
-    sticky = !sticky;
-  } else if ((currentScrollPos <= stickyDistance) && sticky) {
-    // if the user has scrolled back and past the initial navbar position, and
-    // the navbar is sticky, remove the sticky class and flip the sticky flag
-    navbar.classList.remove('sticky');
-    sticky = !sticky;
+    // Make the navbar sticky if the user has scrolled past the navbar
+    // Note: The sticky class will be "removed" even if it is not there
+    // but this has no effect
+    if (hasScrolledPastNavBar) {
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
   }
+
+  // When scrolling, change the scrolling marker flag to true
+  window.onscroll = function() {
+    isScrolling = true;
+  };
+
+  // Check the scrolling marker flag every 25 ms, and update the navbar
+  // state if the user is scrolling, to improve performance
+  setInterval(function() {
+    if (isScrolling) {
+      isScrolling = false;
+      updateNavbarState();
+    }
+  }, 25);
 }
 
-// If scrolling, switch the scrolling flag and wait 100 ms (improves performance)
-setInterval(function() {
-  if(scrolling) {
-    scrolling = !scrolling;
+/* Set up a interval that checks if the user is a desktop user every 1000 ms
+ * If the user is on desktop, initialize the sticky header, and stop the interval
+ *
+ * NOTE: The purpouse of this code is to only load the sticky header javascript
+ * if the user is a deskttop user, and also allow for the user to change from
+ * a small screen to a big screen without realoding the browser */
+
+const initializeStickyHeaderIntervalID = setInterval(function (){
+  // check if the user is on desktop
+  const isDesktopUser = window.outerWidth >= 960;
+
+  // Initialize sticky header code if the user is a desktop user,
+  // and stop the interval
+  if (isDesktopUser) {
+    initializeStickyHeader();
+    window.clearInterval(initializeStickyHeaderIntervalID);
   }
 }, 1000);
 
-// When scrolling, update the navbar state
-window.onscroll = updateNavbarState;
 
 // Mobile menu toggler
 const navbarToggler = document.getElementById('main-nav-toggler');
 
-var toggled = false;
+var isActive = false;
 
 function toggleNavbar() {
-  if (!toggled) {
+  if (!isActive) {
     navbar.classList.add('toggled');
     navbarToggler.classList.add('active');
   } else {
     navbar.classList.remove('toggled');
     navbarToggler.classList.remove('active');
   }
-  toggled = !toggled;
+  isActive = !isActive;
 }
 
-navbarToggler.onclick = function() {
-  toggleNavbar();
-};
+navbarToggler.onclick = toggleNavbar;
