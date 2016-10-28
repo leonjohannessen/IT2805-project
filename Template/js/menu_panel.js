935ebe33182bc19855c33e0432502449f44e4581
linnhd <linnharbodahle@gmail.com>
2016-10-28 12:41:16 +0200

    Javascript for tabbed menu panel added to catering page.

--- Template/js/menu_panel.js
+++ Template/js/menu_panel.js
@@ -1,1 +1,29 @@
 /*The javascript for the tabbed menu panel in the catering page*/
+
+
+function openMenu(evt, menuNumber){
+
+  /*Get all the tabContent-elements*/
+  var content = document.getElementsByClassName("tabContent");
+
+  /*Hide all the menu content*/
+  for(var i = 0; i < content.length ; i++){
+    content[i].style.display = "none";
+  }
+
+  /*Get all the menuLinks */
+  var menuLinks = document.getElementsByClassName("menuLinks");
+
+  /*Go through all menuLinks and remove the active and replace it with nothing*/
+  for(var i = 0; i < menuLinks.length ; i++){
+    menuLinks[i].className = menuLinks[i].className.replace("active"," ");
+  }
+
+  /*Get the menu numbers (from the function) and set them to be shown  */
+  document.getElementById(menuNumber).style.display = "block";
+  /*Add an active class to that number*/
+  evt.currentTarget.className = evt.currentTarget.className + "active";
+}
+
+/* Set Meny 1 to be initially opened by making javascript click on it*/
+document.getElementById("defaultOpen").click();
