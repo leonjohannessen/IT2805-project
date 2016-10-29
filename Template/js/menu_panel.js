2f4511e88c70bebaa8560f667b6e1f0f50aae420
linnhd <linnharbodahle@gmail.com>
2016-10-29 15:23:37 +0200

    Changed the name on the tabbed menu to tabbed panel so that this javascript can be used in the contactpage also

--- Template/js/menu_panel.js
+++ Template/js/menu_panel.js
@@ -1,28 +1,28 @@
-/*The javascript for the tabbed menu panel in the catering page*/
+/*The javascript for the tabbed menu panel in the catering page and the tabbed panel in the contact page*/
 
 
-function openMenu(evt, menuNumber){
+function openPanel(evt, specificTab){
 
   /*Get all the tabContent-elements*/
   var content = document.getElementsByClassName("tabContent");
 
-  /*Hide all the menu content*/
+  /*Hide all the content*/
   for(var i = 0; i < content.length ; i++){
     content[i].style.display = "none";
   }
 
-  /*Get all the menuLinks */
-  var menuLinks = document.getElementsByClassName("menuLinks");
+  /*Get all the panelLinks */
+  var panelLinks = document.getElementsByClassName("panelLinks");
 
-  /*Go through all menuLinks and remove the isActive class if it is there*/
-  for(var i = 0; i < menuLinks.length ; i++){
-    menuLinks[i].classList.remove("isActive");
+  /*Go through all panelLinks and remove the isActive class if it is there*/
+  for(var i = 0; i < panelLinks.length ; i++){
+    panelLinks[i].classList.remove("isActive");
   }
 
-  /*Get the menu number that is clicked and set the content for for example menu3 to be shown by turning it "on" with "block" */
-  document.getElementById(menuNumber).style.display = "block";
+  /*Get the  number that is clicked and set the content for for example menu3 to be shown by turning it "on" with "block" */
+  document.getElementById(specificTab).style.display = "block";
 
-  /*Add a class called isActive to that menu-number that was clicked*/
+  /*Add a class called isActive to that tab-number that was clicked*/
   evt.currentTarget.classList.add("isActive");
 }
 
